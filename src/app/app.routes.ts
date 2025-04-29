237f48e2f6db5eed73e715f9d0c43278531a7895
Felix <felixcesar2078@gmail.com>
2025-04-29 08:47:18 -0500

    agregar componentes de Dashboard y Alumnos, actualizar rutas y desactivar análisis en Angular CLI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/loginComponents/login/login.component';
 import { IndexComponent } from './features/main/layout/index/index.component';
-
+import { DashboardComponent } from './features/main/pages/dashboard/dashboard.component';
+import { AlumnosComponent } from './features/main/pages/alumnos/alumnos.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'index', component:IndexComponent }
+  { 
+    path: 'index', 
+    component: IndexComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'alumnos', component: AlumnosComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
 ];
